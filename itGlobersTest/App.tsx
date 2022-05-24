8eee5b16273b401b5745ffe233b97d526c145ea7
santiago b <sbarraganp19@gmail.com>
2022-05-24 02:11:39 -0500

    fixed children in ImageBackground

--- itGlobersTest/App.tsx
+++ itGlobersTest/App.tsx
@@ -6,24 +6,24 @@ const imageBackground = require('./assets/images/it-globers-bkg1.jpg');
 
 export default function App() {
     return (
-      <View style={styles.container}>
-          <ImageBackground source={imageBackground} style={styles.image}>
+        <View style={styles.container}>
+            <ImageBackground source={imageBackground} style={styles.image}/>
             <Login/>
-          </ImageBackground>
-      </View>
-  );
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
+    container: {
     flex: 1,
     backgroundColor: '#f1f1f1',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    },
     image:{
-        width: '100%',
-        height: '100%',
+        position: 'absolute',
+        width: "100%",
+        height: "100%",
         flex: 1
     }
 });
